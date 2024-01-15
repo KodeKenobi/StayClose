d7ba8343d12e110ce76488af8af351fdcd080c08
KodeKenobi <kodekenobi@gmail.com>
2024-01-15 18:50:43 +0200

    Updated changes

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -1,11 +1,7 @@
-'use client';
+"use client";
 
-import { 
-  FieldErrors, 
-  FieldValues, 
-  UseFormRegister 
-} from "react-hook-form";
-import { BiDollar } from "react-icons/bi";
+import { format } from "path";
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
 
 interface InputProps {
   id: string;
@@ -14,32 +10,24 @@ interface InputProps {
   disabled?: boolean;
   formatPrice?: boolean;
   required?: boolean;
-  register: UseFormRegister<FieldValues>,
-  errors: FieldErrors
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors;
 }
 
 const Input: React.FC<InputProps> = ({
   id,
   label,
-  type = "text", 
-  disabled, 
+  type,
+  disabled,
   formatPrice,
-  register,
   required,
+  register,
   errors,
 }) => {
   return (
     <div className="w-full relative">
       {formatPrice && (
-        <BiDollar
-          size={24}  
-          className="
-            text-neutral-700
-            absolute
-            top-5
-            left-2
-          "
-        />
+        <span className="absolute top-5 text-neutral-700 left-2">R</span>
       )}
       <input
         id={id}
@@ -48,45 +36,46 @@ const Input: React.FC<InputProps> = ({
         placeholder=" "
         type={type}
         className={`
-          peer
-          w-full
-          p-4
-          pt-6 
-          font-light 
-          bg-white 
-          border-2
-          rounded-md
-          outline-none
-          transition
-          disabled:opacity-70
-          disabled:cursor-not-allowed
-          ${formatPrice ? 'pl-9' : 'pl-4'}
-          ${errors[id] ? 'border-rose-500' : 'border-neutral-300'}
-          ${errors[id] ? 'focus:border-rose-500' : 'focus:border-black'}
+            peer
+            w-full
+            p-4
+            pt-6
+            font-light
+            bg-white
+            border-2
+            rounded-md
+            outline-none
+            transition
+            disabled:opacity-70
+            disabled:cursor-not-allowed
+            ${formatPrice ? "pl-9" : "pl-4"}
+            ${errors[id] ? "border-rose-500" : "border-neutral-300"}
+            ${errors[id] ? "focus:border-rose-500" : "focus:border-black"}
+
         `}
       />
-      <label 
+      <label
         className={`
-          absolute 
-          text-md
-          duration-150 
-          transform 
-          -translate-y-3 
-          top-5 
-          z-10 
-          origin-[0] 
-          ${formatPrice ? 'left-9' : 'left-4'}
-          peer-placeholder-shown:scale-100 
-          peer-placeholder-shown:translate-y-0 
-          peer-focus:scale-75
-          peer-focus:-translate-y-4
-          ${errors[id] ? 'text-rose-500' : 'text-zinc-400'}
+            absolute
+            text-md
+            duration-150
+            transform
+            -translate-y-3
+            top-5
+            z-10
+            origin-[0]
+            ${formatPrice ? "left-9" : "left-4"}
+            peer-placeholder-shown:scale-100
+            peer-placeholder-shown:translate-y-0
+            peer-focus:scale-75
+            peer-focus:-translate-y-4
+            ${errors[id] ? "text-rose-500" : "text-zinc-400"}
         `}
       >
         {label}
       </label>
     </div>
-   );
-}
- 
+  );
+};
+
 export default Input;
