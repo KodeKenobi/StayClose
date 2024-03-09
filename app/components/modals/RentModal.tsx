cac287bce09400eacf6f26bb911f081f625a4b9d
KodeKenobi <kodekenobi@gmail.com>
2024-03-09 11:07:42 +0200

    Fixed Vercel Deployment

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -72,7 +72,8 @@ const RentModal = () => {
       dynamic(() => import("../Map"), {
         ssr: false,
       }),
-    [location]
+    // [location]
+    []
   );
 
   const setCustomValue = (id: string, value: any) => {
